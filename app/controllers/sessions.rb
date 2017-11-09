69dcbfc2cf4c5fc793a32624a2052f4c44c85735
Adam Johnson <adamjohnson@Adams-MacBook-Pro.local>
2017-11-09 17:01:38 -0600

    complete registration and login

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,1 +1,21 @@
+get '/sessions/new' do
+	erb :'/sessions/new'
+end
 
+post '/sessions' do
+	@user = User.find_by(username: params[:username])
+
+	if @user && @user.password == params[:password]
+		session[:user_id] = @user.id
+		redirect '/jokes'
+	else
+		@errors = ["That password/username combo don't work."]
+		erb :'/sessions/new'
+	end
+end
+
+
+delete '/sessions' do
+	session[:user_id] = nil
+	redirect '/jokes'
+end
