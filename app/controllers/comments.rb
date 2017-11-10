bbea936a251497d95bd87a1a9421847206c18dc3
Adam Johnson <adamjohnson@Adams-MacBook-Pro.local>
2017-11-10 11:43:24 -0600

    add comment functionality to answer-comments and display all answer-comments

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -3,8 +3,19 @@ post '/jokes/:id/comments' do
 		@comment = Comment.create(description_text: params[:description_text], commentable_type: "Joke", user: current_user, commentable_id: params[:id])
 		redirect "/jokes/#{params[:id]}"
 	else
-		@errors = ["You must be logged in to post."]
+		@errors = ["You must be logged in to post a comment."]
 		erb :'/sessions/new'
 	end
+end
 
+post '/answers/:id/comments' do
+	@answer = Answer.find(params[:id])
+	@joke = @answer.joke
+	if logged_in?
+		@comment = Comment.create(description_text: params[:description_text], commentable_type: "Answer", user: current_user, commentable_id: params[:id])
+		redirect "/jokes/#{@joke.id}"
+	else
+		@errors = ["You must be logged in to post a comment."]
+		erb :'/sessions/new'
+	end
 end
