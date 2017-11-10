834177b95517e8f5d575a4926ebdb53ad817102f
Adam Johnson <adamjohnson@Adams-MacBook-Pro.local>
2017-11-09 18:53:36 -0600

    add joke id page

--- app/controllers/jokes.rb
+++ app/controllers/jokes.rb
@@ -1,4 +1,10 @@
 get '/jokes' do
-  @jokes = Joke.all
-  erb :'jokes/index'
+  	@jokes = Joke.all
+  	erb :'jokes/index'
+end
+
+
+get '/jokes/:id' do	
+	@joke = Joke.find(params[:id])
+	erb :'/jokes/show'
 end
