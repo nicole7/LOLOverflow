b2f1d3afb6f05e34751ed6c355302231ca45c029
Karlie Romas <karlie.romas@gmail.com>
2017-11-10 12:14:39 -0600

    got best punchline to work with form and route

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,5 +1,13 @@
-post '/jokes/:id/answers' do 
-	@joke = Joke.find(params[:id])
-	@answer = Answer.create(user_id: current_user.id, joke_id: @joke.id, description_text: params[:description_text], vote_tally: 0)
-	redirect "/jokes/#{@joke.id}"
+post '/jokes/:id/answers' do
+  @joke = Joke.find(params[:id])
+  @answer = Answer.create(user_id: current_user.id, joke_id: @joke.id, description_text: params[:description_text], vote_tally: 0)
+  redirect "/jokes/#{@joke.id}"
+end
+
+put '/answers/:id' do
+  @answer = Answer.find(params[:id])
+  @joke = @answer.joke
+  @joke.best_answer_id = @answer.id
+  @joke.save
+  redirect "/jokes/#{@joke.id}"
 end
